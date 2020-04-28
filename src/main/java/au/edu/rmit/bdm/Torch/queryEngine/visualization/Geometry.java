e1df4ddad86c86c9b728b78f82472b76332ee7d4
wendingp <wendingp@gmail.com>
2020-04-28 23:23:09 +0800

    Warn null in Geometry

--- src/main/java/au/edu/rmit/bdm/Torch/queryEngine/visualization/Geometry.java
+++ src/main/java/au/edu/rmit/bdm/Torch/queryEngine/visualization/Geometry.java
@@ -1,23 +1,27 @@
 package au.edu.rmit.bdm.Torch.queryEngine.visualization;
 
 import au.edu.rmit.bdm.Torch.base.model.TrajEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 class Geometry {
+    private static Logger logger = LoggerFactory.getLogger(Geometry.class);
 
-    String type = "LineString";
+    // String type = "LineString";
 
     //lng, lat
     //order is vital
     double[][] coordinates;
 
     Geometry(List<TrajEntry> path) {
-
-        int pathLen = path == null ? 0 : path.size();
-        coordinates = new double[pathLen][2];
-
-        for (int i = 0; i < pathLen; i++) {
+        if (path == null) {
+            logger.warn("path for Geometry is null");
+            return;
+        }
+        coordinates = new double[path.size()][2];
+        for (int i = 0; i < path.size(); i++) {
             coordinates[i][0] = path.get(i).getLng();
             coordinates[i][1] = path.get(i).getLat();
         }
